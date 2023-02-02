01b35f20caddb02a14125196729966f383ffb75f
Raiyan-Noor <raiyannoor@iut-dhaka.edu>
2023-02-02 12:06:36 +0600

    bug fixed in App

--- Server/App.js
+++ Server/App.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port 8000`)
 })
 
 console.log("Hello World")
